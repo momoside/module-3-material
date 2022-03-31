3abd4ee3b382e79a3e7e9b21bcdd236316554ba7
Rudiyanto <rudi.yanto@gojek.com>
2022-03-31 15:42:11 +0700

    add userInfo component

---
+++ src/components/userInfo/index.js
@@ -0,0 +1,11 @@
+export default function UserInfoComponent() {
+  return (
+    <div className="users__profile">
+      <div className="users__avatar" />
+      <div className="users__info">
+        <div className="users__name">Rudiyanto</div>
+        <div className="users__status--active">online</div>
+      </div>
+    </div>
+  );
+}
