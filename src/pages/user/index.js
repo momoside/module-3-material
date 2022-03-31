b076ba38a8ae5ff17c64702a184f0e28a002965d
Rudiyanto <rudi.yanto@gojek.com>
2022-03-31 15:42:34 +0700

    integrate userForm and userInfo component

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 import EmptyStateComponent from "../../components/emptyState";
 import axios from "axios";
+import UserInfoComponent from "../../components/userInfo";
+import UserFormComponent from "../../components/userForm";
 
 export default function UserPage() {
   const [users, setUsers] = useState([]);
@@ -26,7 +28,6 @@ export default function UserPage() {
     fetch();
   }, []); //only first render
 
-  console.log("users", users);
   return (
     <div className="container">
       <div className="users__box">
@@ -37,7 +38,7 @@ export default function UserPage() {
               {users.map((user) => {
                 const isActive = user.status !== "inactive";
                 return (
-                  <li key={user.id}>
+                  <li key={user.id} onClick={() => {}}>
                     {user.name}
                     <div className={`users__status--${user.status}`}>
                       {isActive ? "online" : "offline"}
@@ -49,63 +50,11 @@ export default function UserPage() {
           </div>
         </div>
         <div className="users__box--right">
-          <EmptyStateComponent />
+          {/* <EmptyStateComponent /> */}
+          <UserInfoComponent />
+          <UserFormComponent />
         </div>
       </div>
     </div>
   );
 }
-
-// class UserPage extends React.Component {
-//   state = {
-//     data: [
-//       {
-//         id: 213123,
-//         name: "Rudi",
-//       },
-//       {
-//         id: 123123213,
-//         name: "Wisesa",
-//       },
-//     ],
-//   };
-
-//   async componentDidMount() {
-//     const { data } = await axios.get("https://gorest.co.in/public/v2/users");
-//     this.setState({ data });
-//   }
-
-//   render() {
-//     const { data } = this.state;
-
-//     return (
-//       <div className="container">
-//         <div className="users__box">
-//           <div className="users__box--left">
-//             <div className="users__title">User List</div>
-//             <div className="users__list">
-//               <ul>
-//                 {data.map((user) => {
-//                   const isActive = user.status !== "inactive";
-//                   return (
-//                     <li key={user.id}>
-//                       {user.name}
-//                       <div className={`users__status--${user.status}`}>
-//                         {isActive ? "online" : "offline"}
-//                       </div>
-//                     </li>
-//                   );
-//                 })}
-//               </ul>
-//             </div>
-//           </div>
-//           <div className="users__box--right">
-//             <EmptyStateComponent />
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default UserPage;
